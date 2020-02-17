1d68b8789a8bf65a7147c2ff6f9998177c8bd28d
Michael <mklosterboer@gmail.com>
2020-02-16 20:08:38 -0600

    hook up Resolution to App component

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -6,6 +6,7 @@ import ResolutionsSchema from "../../api/resolutions/Resolutions.graphql";
 const testSchema = `
     type Query {
         hi: String
+        resolutions: [Resolution]
     }
 `;
 
@@ -15,6 +16,18 @@ const resolvers = {
     Query: {
         hi() {
             return "Hello from GraphQL";
+        },
+        resolutions() {
+            return [
+                {
+                    _id: "asdfasdf",
+                    name: "Get stuff done!"
+                },
+                {
+                    _id: "asdfasdfasdf",
+                    name: "Get more stuff done!"
+                }
+            ];
         }
     }
 };
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,12 +3,29 @@ import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
 const App = ({ data }) => {
-    return <div>{data.hi}</div>;
+    if (data.loading) {
+        return <>Loading...</>;
+    }
+
+    return (
+        <>
+            <h1>{data.hi}</h1>
+            <ul>
+                {data.resolutions.map(resolution => (
+                    <li key={resolution._id}>{resolution.name}</li>
+                ))}
+            </ul>
+        </>
+    );
 };
 
 const hiQuery = gql`
     {
         hi
+        resolutions {
+            _id
+            name
+        }
     }
 `;
 
